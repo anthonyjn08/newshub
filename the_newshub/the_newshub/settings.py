b5cb4afce5e6f48d22d343c59ba891a42f674c76
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-10-25 11:41:03 +0100

    Update settigs.py

--- the_newshub/the_newshub/settings.py
+++ the_newshub/the_newshub/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = "django-insecure-*!i95#qyp)vxm)g0d6!p&*qqi_7yz%v%sh6_ujw$bo+3$t$%7p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".direct.labs.play-with-docker.com",
+                 "localhost",
+                 "127.0.0.1"]
 
 
 # Application definition
